24696bfea66b67d9e7c3ce88135ef0f9cf97be14
Liran <liran77771@walla.co.il>
2020-04-06 15:45:22 +0300

    Updates

---
+++ precache-manifest.427a8aedf22f6cb7f7f2a4450b115098.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4145227bd886597102c1c2ab77a31b2",
+    "url": "/omerz138/oritzit-taxindex.html"
+  },
+  {
+    "revision": "68b41518cae39dd09174",
+    "url": "/omerz138/oritzit-taxstatic/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "23a1c978ff11167a944e",
+    "url": "/omerz138/oritzit-taxstatic/css/main.0475828d.chunk.css"
+  },
+  {
+    "revision": "68b41518cae39dd09174",
+    "url": "/omerz138/oritzit-taxstatic/js/2.3e413514.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/omerz138/oritzit-taxstatic/js/2.3e413514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a1c978ff11167a944e",
+    "url": "/omerz138/oritzit-taxstatic/js/main.9bbe98cf.chunk.js"
+  },
+  {
+    "revision": "fbdd85654a6d6c6cbdab",
+    "url": "/omerz138/oritzit-taxstatic/js/runtime-main.9271e8ca.js"
+  },
+  {
+    "revision": "9eb83f7c4362652a3cb3d42eef426a60",
+    "url": "/omerz138/oritzit-taxstatic/media/cover-logo.9eb83f7c.jpg"
+  },
+  {
+    "revision": "717173aab65f4baa61b5cfbf7435526b",
+    "url": "/omerz138/oritzit-taxstatic/media/orit.717173aa.jpg"
+  }
+]);
